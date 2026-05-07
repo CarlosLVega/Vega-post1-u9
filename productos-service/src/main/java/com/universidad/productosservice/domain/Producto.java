8e8c42db0fceb731a439764dbed562ddcb8f5f1e
Carlos Leonardo Vega Castro <cvegac684@gmail.com>
2026-05-06 20:26:09 -0500

    Solución Lombok

--- productos-service/src/main/java/com/universidad/productosservice/domain/Producto.java
+++ productos-service/src/main/java/com/universidad/productosservice/domain/Producto.java
@@ -6,15 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "productos")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Producto {
 
 	@Id
